328c015f390b746a1a07f85d518fdf51e8f5e6ff
virender291 <virenderchawla123@gmail.com>
2022-04-03 23:26:41 +0500

    Detail Screen updated

--- Screens/DetailScreen.js
+++ Screens/DetailScreen.js
@@ -1,13 +1,61 @@
 import React from 'react';
-import { View,Text } from 'react-native';
+import {View, Text, SafeAreaView, Image,ScrollView} from 'react-native';
+import {NavigationContainer, useNavigation} from '@react-navigation/native';
+import {Component} from 'react/cjs/react.production.min';
+import {Card, ListItem, Button, Icon} from 'react-native-elements';
 
 function DetailScreen(props) {
     return (
+        <ScrollView>
         <View>
-            <Text>
-                Virender
-            </Text>
-        </View>
+        <Card title="CARD WITH DIVIDER">
+        <Image
+          source={require('../pictures/images/1.png')}
+          style={{
+            width: 350,
+            height: 200,
+            borderRadius: 20,
+            marginBottom: 15,
+          }}></Image>
+        <Button
+          style={{}}
+          onPress={() => {
+            navigation.navigate('Detail');
+          }}
+          title="Add to Cart"
+          color="#841584"
+        />
+      </Card>
+      <Card>
+          <Text>
+              Product Detail
+          </Text>
+          <Text>
+              name:"Play Station 4"
+          </Text>
+          <Text>
+              Price:500/Day
+          </Text>
+
+      </Card>
+      <Card>
+          <Text>
+             Description
+          </Text>
+          <Text>
+            this is ps4 hhusdfjfsjdoosdhcisdichisdhcisdhichdsichisdcisdibsidvbdbkbxkd
+          </Text>
+      </Card>
+      <Card>
+          <Text>
+             Reviews
+          </Text>
+          <Text>
+            this is ps4 hhusdfjfsjdoosdhcisdichisdhcisdhichdsichisdcisdibsidvbdbkbxkd
+          </Text>
+      </Card>
+      </View>
+      </ScrollView>
     );
 }
 
